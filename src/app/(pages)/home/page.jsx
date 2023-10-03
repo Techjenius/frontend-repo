121c97699cd016e6fefd70afe01c872748ce2327
UncleP04 <philipnssien@gmail.com>
2023-10-03 10:27:39 +0100

    initial push

---
+++ src/app/(pages)/home/page.jsx
@@ -0,0 +1,14 @@
+'use clients'
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className='p-4'>
+            <p className='text-[2em]'>
+                Hello TechGenius!, this is the dashboard.
+            </p>
+        </div>
+    )
+};
+
+export default Dashboard;
